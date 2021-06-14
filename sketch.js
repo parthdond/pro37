9c70f380dfba8e2600d635a02adc94a571519d53
parthdondgmail.com <parthdond2006>
2021-06-14 12:07:35 +0530

    database added

--- sketch.js
+++ sketch.js
@@ -1,21 +1,63 @@
-//Create variables here
+var dog, happyDog, database, foodS, foodStock , position;
+var backgroundImg, dogImg;
 
 function preload()
 {
-	//load images here
-}
-
-function setup() {
-	createCanvas(800, 700);
+  dogImg = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png");  
+  
   
 }
 
 
-function draw() {  
+function setup(){
+    createCanvas(900,500);
+    database= firebase.database();
 
-  drawSprites();
-  //add styles here
+    dog = createSprite(450,300,50,50);
+    dog.addImage("dog",dogImg);
+    dog.scale=0.5  ;
 
+    foodStock =  database.ref('food');
+    foodStock.on("value",readStock)
+}
+
+function draw(){
+    background("lightblue");
+    
+   
+    if(keyWentDown("space")){
+      dog.addImage("happy",happyDog);
+       WriteStock(foodS)
+       
+    }
+    
+
+   
+    
+    drawSprites();
+    textSize(25);
+    fill("black");
+    text("food remaining:"+ foodS,170,80);
+    text("press space to feed the dog!" ,170 , 120 )
+    
+}
+
+function WriteStock(petFOOD){
+    if(petFOOD<=0){
+        petFOOD=0
+           
+    }
+    else{
+        petFOOD=petFOOD-1;
+    }
+    database.ref('/').update({
+        food:petFOOD
+    })
+}
+function readStock(data){
+    foodS = data.val();
+    
 }
 
 
